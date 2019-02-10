1d508767f36996d5f2addfd60fb47cf9851b461a
DESKTOP-M6C4JDD\Nikita <nandreev@colectivosvip.com>
2019-02-10 22:18:10 +0200

    first init

--- src/main/java/ciklum/task/impl/IUserServiceImpl.java
+++ src/main/java/ciklum/task/impl/IUserServiceImpl.java
@@ -79,7 +79,6 @@ public class IUserServiceImpl implements IUserService {
                     .collect(Collectors.toList())
             )
         );
-
         return userAllUsersByGroupIdMap;
     }
 }
